43571a95d19b7d1203b61c237dc09ceed5399134
Yair Cohen <yair.cohen.mail@gmail.com>
2024-07-31 18:46:53 +0300

    base routes

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -12,3 +12,21 @@ export type IGenre = {
     name: string;
     picture: Buffer;
 };
+
+export type SelectionTypes = 'artists' | 'genres';
+export type ContactMethod = 'sms' | 'email';
+export type Selection = { [key in SelectionTypes]: string[] };
+export type IVoter = {
+    contactMethod: ContactMethod;
+    didVote: boolean;
+    selection: Selection;
+};
+
+export type IEvent = {
+    id: string;
+    name: string;
+    color: string;
+    genres: string[];
+    voters: Record<string, IVoter>;
+    votes: { [key in SelectionTypes]: Record<string, number> };
+};
